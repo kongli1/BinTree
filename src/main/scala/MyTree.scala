5abd4a2451577935adba3c5943745f79d23bd40c
liRenhao <renhao.li@bjyada.com>
2015-09-14 22:20:25 +0800

    Bug修改

--- src/main/scala/MyTree.scala
+++ src/main/scala/MyTree.scala
@@ -3,27 +3,30 @@ import scala.collection.mutable.ArrayBuffer
 
 case class BinTree[T](value: T, left: Option[BinTree[T]], right: Option[BinTree[T]])
 
+case class CoordTree[T](value: T, dist: Int, depth: Int)
+
 object MyTree {
   def topView[T <: Any](root: BinTree[T]): Seq[T] = {
-    val map = mutable.Map.empty[Int, T]
-    distView[T](root, map, 0)
+    val map = mutable.Map.empty[Int, CoordTree[T]]
+    distView[T](root, map, 0, 0)
     var array = new ArrayBuffer[T]
     map.keySet.toList.sorted foreach {
-      case key: Int => array += map.get(key).get
+      case key: Int => array += map.get(key).get.value
     }
     array.seq
   }
 
-  private def distView[T](root: BinTree[T], map: mutable.Map[Int, T], dist: Int): Unit = {
-    if (map.get(dist).isEmpty) {
-      map += (dist -> root.value)
+  private def distView[T](root: BinTree[T], map: mutable.Map[Int, CoordTree[T]], dist: Int, depth: Int): Unit = {
+    if (map.get(dist).isEmpty || depth < map.get(dist).get.depth) {
+      map += (dist -> new CoordTree(root.value, dist, depth))
     }
+
     root.right match {
-      case Some(right) => distView(right, map, dist + 1)
+      case Some(right) => distView(right, map, dist + 1, depth + 1)
       case None =>
     }
     root.left match {
-      case Some(left) => distView(left, map, dist - 1)
+      case Some(left) => distView(left, map, dist - 1, depth + 1)
       case None =>
     }
   }
@@ -31,20 +34,26 @@ object MyTree {
   def main(args: Array[String]) {
     val root = BinTree(3,
       Some(BinTree(5,
+        None,
         Some(BinTree(1,
-          None,
-          Some(BinTree(9, None, None)))),
-        Some(BinTree(4, None, None)))),
+          Some(BinTree(4,
+            None,
+            Some(BinTree(10,
+              None,
+              Some(BinTree(9, None, None)))))),
+          None)))),
       Some(BinTree(2,
-        Some(BinTree(6, None, None)),
-        Some(BinTree(7,
-          Some(BinTree(8, None, None)),
-          None)))))
+        Some(BinTree(6,
+          Some(BinTree(7,
+            Some(BinTree(8, None, None)),
+            None)),
+          None)),
+        None)))
 
-    val map = mutable.Map.empty[Int, Int]
-    distView(root, map, 0)
-    map.toList.sorted foreach {
-      case (key, value) => println(key + " = " + value)
+    val map = mutable.Map.empty[Int, CoordTree[Int]]
+    distView(root, map, 0, 0)
+    map.keySet.toList.sorted foreach {
+      case key => println(key + " = " + map.get(key).get.value)
     }
   }
 }
